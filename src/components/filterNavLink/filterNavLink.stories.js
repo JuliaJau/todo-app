b10137f80479e1a871aee5d8e5b82fc31965323f
JuliaJau <jautzj@gmail.com>
2021-07-25 15:23:56 +0200

    Set color and font-weight

--- src/components/filterNavLink/filterNavLink.stories.js
+++ src/components/filterNavLink/filterNavLink.stories.js
@@ -1,9 +1,11 @@
 import "./filterNavLink.css";
 import filterNavLink from "./filterNavLink.html";
+import filterNavLinkactive from "./filterNavLink-active.html";
 
 export default {
   title: "Components/FilterNavLink",
   parameters: { layout: "fullscreen" },
 };
 
-export const FilterNavLink = () => filterNavLink;
+export const link = () => filterNavLink;
+export const linkactive = () => filterNavLinkactive;
