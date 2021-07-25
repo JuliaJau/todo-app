ae51005101d2c5c754f74c0277a2cb2b6e780722
JuliaJau <jautzj@gmail.com>
2021-07-25 13:33:20 +0200

    Set structure filterNavLink

---
+++ src/components/filterNavLink/filterNavLink.stories.js
@@ -0,0 +1,9 @@
+import "./filterNavLink.css";
+import filterNavLink from "./filterNavLink.html";
+
+export default {
+  title: "Components/FilterNavLink",
+  parameters: { layout: "fullscreen" },
+};
+
+export const FilterNavLink = () => filterNavLink;
