98cdcbc5df50e202241add29f97c0267243eea96
JuliaJau <jautzj@gmail.com>
2021-07-26 16:28:38 +0200

    Delete notifbutton

--- src/components/notifbutton/notifbutton.stories.js
+++
@@ -1,9 +0,0 @@
-import "./notifbutton.css";
-import notifbutton from "./notifbutton.html";
-
-export default {
-  title: "Components/Notifbutton",
-  parameters: { layout: "fullscreen" },
-};
-
-export const Notifbutton = () => notifbutton;
