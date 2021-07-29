52a579231a3325e86ff8387d92684fd4d4971e32
JuliaJau <jautzj@gmail.com>
2021-07-29 13:55:35 +0200

    Add taskform functionality

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-const message = "Hello Students!";
+const taskList = document.querySelector(".taskList");
+const potentialTask = {
+  title: "Get groceries",
+  date: "Tomorrow",
+  isDone: true,
+};
 
-alert(message);
+const taskFromObject = createTaskListItem(potentialTask);
+
+taskList.append(taskFromObject);
+
+function createTaskListItem(task) {
+  const taskListItem = document.createElement("label");
+  const input = document.createElement("input");
+  const span = document.createElement("span");
+
+  taskListItem.className = "taskItem";
+
+  input.className = "taskItem__checkbox";
+  input.type = "checkbox";
+  input.setAttribute("name", "tasks");
+  input.checked = task.isDone;
+
+  span.className = "taskItem__labelText";
+  span.innerText = task.title;
+
+  taskListItem.append(input, span);
+
+  return taskListItem;
+}
